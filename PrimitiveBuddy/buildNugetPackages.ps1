1ab7a48a32811bd8abc8d3aa4164a344a6b38d0f
Dan Manning <dmanning23@hotmail.com>
2016-04-04 21:24:40 -0500

    ios nuget pacakges

--- PrimitiveBuddy/buildNugetPackages.ps1
+++ PrimitiveBuddy/buildNugetPackages.ps1
@@ -1,3 +1,3 @@
 nuget pack .\PrimitiveBuddy.Android\PrimitiveBuddy.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-
-nuget pack .\PrimitiveBuddy.WindowsGL\PrimitiveBuddy.WindowsGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\PrimitiveBuddy.iOS\PrimitiveBuddy.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\PrimitiveBuddy.DesktopGL\PrimitiveBuddy.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
