08a05e351806e9f0857b3bca9186fad85d57726c
Dan Manning <dmanning23@hotmail.com>
2016-04-24 19:04:01 -0500

    moved into pcl, added winuni

--- PrimitiveBuddy/buildNugetPackages.ps1
+++ PrimitiveBuddy/buildNugetPackages.ps1
@@ -1,3 +1,2 @@
-nuget pack .\PrimitiveBuddy.Android\PrimitiveBuddy.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\PrimitiveBuddy.iOS\PrimitiveBuddy.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\PrimitiveBuddy.DesktopGL\PrimitiveBuddy.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\PrimitiveBuddy.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget push *.nupkg
