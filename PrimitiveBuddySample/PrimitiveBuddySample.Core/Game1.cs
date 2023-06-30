d55385228ec7ef5542391891810a21d4406abb3a
Dan Manning <dmanning@oceaneering.com>
2023-06-30 14:48:22 -0500

    added sample

---
+++ PrimitiveBuddySample/PrimitiveBuddySample.Core/Game1.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using PrimitiveBuddy;
+
+namespace PrimitiveBuddySample.WindowsGL
+{
+	/// <summary>
+	/// This is the main type for your game.
+	/// </summary>
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		Primitive _prim;
+		Primitive _ropePrim;
+		KeyboardState _keys;
+		float step = 200f;
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			_keys = new KeyboardState();
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+
+			base.Initialize();
+			//_prim = new Primitive(graphics.GraphicsDevice, spriteBatch);
+			//_prim.NumCircleSegments = 3;
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			// TODO: use this.Content to load your game content here
+			var rope = Content.Load<Texture2D>("rope");
+			//_ropePrim = new Primitive(graphics.GraphicsDevice, spriteBatch);
+			_ropePrim = new Primitive(rope, spriteBatch);
+			_ropePrim.Thickness = 6;
+
+			_prim = new Primitive(graphics.GraphicsDevice, spriteBatch);
+			//_prim = new Primitive(rope, spriteBatch);
+			_prim.Thickness = 6;
+		}
+
+		/// <summary>
+		/// UnloadContent will be called once per game and is the place to unload
+		/// game-specific content.
+		/// </summary>
+		protected override void UnloadContent()
+		{
+			if (null != _prim)
+			{
+				_prim.Dispose();
+				_prim = null;
+			}
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			var curKeys = Keyboard.GetState();
+			if (_keys.IsKeyUp(Keys.A) && curKeys.IsKeyDown(Keys.A))
+			{
+				_prim.NumCircleSegments = _prim.NumCircleSegments + 1;
+			}
+			else if (_keys.IsKeyUp(Keys.S) && curKeys.IsKeyDown(Keys.S))
+			{
+				_prim.NumCircleSegments = _prim.NumCircleSegments - 1;
+			}
+			_keys = curKeys;
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			spriteBatch.Begin();
+
+			_prim.Circle(new Vector2(128f, 128f), 64f, Color.White);
+			_prim.Pie(new Vector2(256f, 256f), 64f, MathHelper.PiOver2, MathHelper.PiOver2, Color.White);
+
+			_prim.Line(new Vector2(300, 200), new Vector2(500, 200), Color.White);
+			_prim.SineWave(new Vector2(300, 300), new Vector2(400, 400), 20f, 30f, Color.Red);
+			_prim.SineWave(new Vector2(300, 400), new Vector2(400, 550), 20f, 30f, Color.White);
+			_prim.SineWave(new Vector2(200, 400), new Vector2(300, 300), 20f, 30f, Color.Green);
+
+			_prim.Rectangle(new Rectangle(64, 64, 128, 128), Color.Black);
+
+			spriteBatch.End();
+
+			spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.LinearWrap);
+
+			_ropePrim.SineWave(new Vector2(step, 100), new Vector2(200, 100), 20f, 30f, Color.White);
+			step += 2f;
+			if (step >= 1000)
+			{
+				step = 200f;
+			}
+
+			spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
+}
--- PrimitiveBuddySample/PrimitiveBuddySample.WindowsGL/Properties/AssemblyInfo.cs
+++ PrimitiveBuddySample/PrimitiveBuddySample.WindowsGL/Properties/AssemblyInfo.cs
@@ -2,35 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PrimitiveBuddy.Bridge")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PrimitiveBuddySample.WindowsGL")]
+[assembly: AssemblyProduct("PrimitiveBuddySample.WindowsGL")]
 [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PrimitiveBuddy.Bridge")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("24640627-0064-478a-89aa-ad55f1924b2c")]
+[assembly: Guid("66b1bd7b-f0c4-46e8-8f73-8a0fda9a396f")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.4.0.456")]
+[assembly: AssemblyFileVersion("3.4.0.456")]
